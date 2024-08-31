369ae0a9dfd4f06ccdc447a51b9352746a096325
mahakamal-e <mahakamal09@gmail.com>
2024-08-31 12:19:33 -0700

    upload files

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,28 +1,28 @@
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 const AppController = {
-    async getStatus(req, res) {
-        try {
-            const redisAlive = redisClient.isAlive();
-            const dbAlive = dbClient.isAlive();
+  async getStatus(req, res) {
+    try {
+      const redisAlive = redisClient.isAlive();
+      const dbAlive = dbClient.isAlive();
 
-            res.status(200).json({ redis: redisAlive, db: dbAlive });
-        } catch (error) {
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-
-    async getStats(req, res) {
-        try {
-            const usersCount = await dbClient.nbUsers();
-            const filesCount = await dbClient.nbFiles();
-
-            res.status(200).json({ users: usersCount, files: filesCount });
-        } catch (error) {
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
+      res.status(200).json({ redis: redisAlive, db: dbAlive });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  },
+
+  async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
 };
 
 export default AppController;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import routes from './routes';
+import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use('/', router);
+app.use(routes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
